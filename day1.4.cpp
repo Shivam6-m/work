1f3b8f92c64d443a748884a960461c98ca471276
Shivam chaturvedi <sc0712744@gmail.com>
2026-02-04 13:41:39 +0530

    g

--- day1.4.cpp
+++ day1.4.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
-
+int reverse(int n){
+    int dig;
+    int rev= 0;
+    while (n != 0)
+    {
+        dig
+    }
+    
+}
 int main() {
     int num; 
-    int sum = 0;
-    int digit;
+    
     cout << "Enter a number: ";
     cin >> num;
-while(num!=0){
-        digit = num % 10;   
-        sum += digit;       
-        num /= 10;          
-    }
-    cout <<"The sum of the digits is"<< sum << endl;
-    cout<<abs(sum)<<endl;
-    return 0;
+    int rev = reverse(num);
+return 0;
 }
